4a81dc8c5fb42c67d3a955af383107159498a986
Marijn Pool <15004290+ph00lt0@users.noreply.github.com>
2019-12-01 21:30:13 +0100

    add SESSION information after login

--- api/manager/login.php
+++ api/manager/login.php
@@ -1,5 +1,12 @@
 <?php
 
+session_start();
+
+if( $_SESSION['managerID'] ) {
+    print_r("Already logged in");
+    exit();
+}
+
 require_once(__DIR__.'../../readonly-connection.php');
 
 if( empty($_POST['password']) ){
@@ -18,19 +25,31 @@ $sPassword = $_POST['password'];
 $db = new DB();
 $con = $db->connect();
 if ($con) {
-    $results = array();
-
-    $statement = $con->prepare("SELECT `cPassword` FROM tmanager WHERE `cUsername` = '$sUsername' 
-                                         OR `cEmail` = '$sUsername'  LIMIT 1");
+    $statement = $con->prepare("SELECT * FROM tmanager WHERE `cUsername` = '$sUsername' 
+                                         OR `cEmail` = '$sUsername' LIMIT 1");
     $statement->execute();
 
-    $sPasswordChecksum = $statement->fetch()['cPassword'];
+    $results = $statement->fetch();
+    $sPasswordChecksum = $results['cPassword'];
 
     if (!password_verify($sPassword, $sPasswordChecksum)) {
         print_r("Incorrect credentials");
         exit();
     }
+
+    if ($results['dCancelled'] !== NULL) {
+        print_r("Incorrect credentials");
+        exit();
+    }
+
     print_r("Correct credentials");
+    print_r($statement->fetch()['nManagerID']);
+    $_SESSION['managerID'] =  $results['nManagerID'];
+    $_SESSION['firstName'] =  $results['cFirstname'];
+    $_SESSION['surname'] =  $results['cSurname'];
+    $_SESSION['email'] =  $results['cEmail'];
+    $_SESSION['username'] =  $results['cUsername'];
+    $_SESSION['phoneNumber'] = $results['cPhoneNumber'];
 
     $stmt = null;
     $db->disconnect($con);
