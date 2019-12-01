f1c038b3030f13141f14fbb6f930b10ed83dad75
Marijn Pool <15004290+ph00lt0@users.noreply.github.com>
2019-12-01 16:23:42 +0100

    read api works

--- api/readonly-connection.php
+++ api/readonly-connection.php
@@ -12,7 +12,7 @@ class DB {
         // driver options
         $options = [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,  //removes index data
             PDO::ATTR_EMULATE_PREPARES => false
         ];
         try {
