3dce833c9f564013b23aacb4a19e18f033a76d1b
ip3ly5 <ip3ly5@gmail.com>
2019-12-01 18:14:35 +0100

    Create Delete.php
    Need to add login system still, it successfully deletes cocktail via ID

---
+++ api/cocktail/delete.php
@@ -0,0 +1,25 @@
+<?php
+require_once(__DIR__.'../../admin-connection.php');
+require_once(__DIR__.'../../functions.php');
+$db = new DB();
+$con = $db->connect();
+
+$iCocktailID = $_POST['cocktailID'];
+
+// $_SESSION['managerID'] = 11;
+
+if(!$_POST){
+    sendErrorMessage( 'Invalid origin [!$_POST]' , __LINE__ ); 
+}
+
+// if($_SESSION['managerID'] != $iManagerID ){ 
+//     sendErrorMessage( 'You are not authenticated' , __LINE__ ); 
+// }
+
+
+if ($con) {
+    $cQuery = "DELETE FROM `tcocktail` WHERE `tcocktail`.`nCocktailID` = '$iCocktailID'";    
+    $stmt = $con->query($cQuery);
+    $stmt = null;
+    $db->disconnect($con);
+}
