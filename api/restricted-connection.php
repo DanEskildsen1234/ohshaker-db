5f612f16384cf3bb266732793757d1dd1bbb160d
Marijn Pool <15004290+ph00lt0@users.noreply.github.com>
2019-12-01 15:33:07 +0100

    replace pdo connections

---
+++ api/restricted-connection.php
@@ -0,0 +1,31 @@
+<?php
+
+class DB {
+    public function connect()
+    {
+        $host = '127.0.0.1';
+        $db = 'ohshaker';
+        $user = 'ohshaker_restricted';
+        $pass = 'restricted123';
+        $charset = 'utf8mb4';
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        // driver options
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false
+        ];
+        try {
+            $pdo = new PDO($dsn, $user, $pass, $options);
+        } catch (PDOException $oException) {
+            echo 'Connection unsuccessful';
+            die('Connection unsuccessful: ' . $pdo->connect_error());
+        }
+
+        return($pdo);
+    }
+
+    public function disconnect($pdo) {
+        $pdo = null;
+    }
+}
