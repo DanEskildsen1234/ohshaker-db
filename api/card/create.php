724aa8fcc848af0e2ba67d2f3456fcd9629f4f05
DanEskildsen <DanEskildsen1234@users.noreply.github.com>
2019-12-02 15:22:20 +0100

    added regex validation for date and IBAN

--- api/card/create.php
+++ api/card/create.php
@@ -8,57 +8,41 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 
 session_start();
 
-if(empty($_SESSION['managerID'])){
-    echo sendErrorMessage('No user logged in', __LINE__);
-    exit;
+$aExpectedFields =
+    array("expiration", "CCV", "IBAN");
+foreach ($aExpectedFields as $field) {
+    if( empty($_POST["$field"]) ) {
+        sendErrorMessage( "$field is required", __LINE__ );
+    }
 }
 
-if(empty($_POST['expiration'])){
-    echo sendErrorMessage('Expiration date is required', __LINE__);
-    exit;
-}
-
-if(empty($_POST['CCV'])){
-    echo sendErrorMessage('CCV is required', __LINE__);
-    exit;
-}
-
-if(empty($_POST['IBAN'])){
-    echo sendErrorMessage('IBAN is required', __LINE__);
-    exit;
-}
-
-$sManagerID = $_SESSION['managerID'];
-$sExpiration = $_POST['expiration'];
-$sCCV = $_POST['CCV'];
+$iManagerID = (int)htmlspecialchars($_SESSION['managerID']);
+$sExpiration = htmlspecialchars($_POST['expiration']);
+$iCCV = (int)htmlspecialchars($_POST['CCV']);
 $sIBAN = $_POST['IBAN'];
 
-if (preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0]|3[0])$/",$sExpiration)) {
-    echo sendErrorMessage('Expiration date must be a valid number', __LINE__);
+// https://stackoverflow.com/questions/13194322/php-regex-to-check-date-is-in-yyyy-mm-dd-format
+if (!preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0{2})$/", $sExpiration)) {
+    echo sendErrorMessage('Expiration date must be a valid date', __LINE__);
 }
 
-if(!ctype_digit(($_POST['CCV']))){
-    echo sendErrorMessage('CCV must be a valid number', __LINE__);
-    exit;
-}
-
-if (strlen($_POST["CCV"]) != 3) {
+if (strlen($iCCV) != 3) {
     echo sendErrorMessage('CCV must be exactly 3 numbers', __LINE__);
-    exit;
 }
 
-// sanitise IBAN
-
-if (strlen($_POST["IBAN"]) != 18) {
+if (strlen($sIBAN) != 18) {
     echo sendErrorMessage('IBAN must be exactly 18 charecters', __LINE__);
-    exit;
+}
+
+if (!preg_match("/DK\d{16}$/", $sIBAN)) {
+    echo sendErrorMessage('IBAN must be valid', __LINE__);
 }
 
 $db = new DB();
 $con = $db->connect();
 
 if ($con) {
-$scQuery = "INSERT INTO tcreditcard (`nManagerID`, `dExpiration`, `cCCV`, `cIBAN`) VALUES ('$sManagerID', '$sExpiration', '$sCCV', '$sIBAN')";
+$scQuery = "INSERT INTO tcreditcard (`nManagerID`, `dExpiration`, `cCCV`, `cIBAN`) VALUES ('$iManagerID', '$sExpiration', '$iCCV', '$sIBAN')";
 $statement = $con->prepare($scQuery);
 $statement->execute();
 $db->disconnect($con);
