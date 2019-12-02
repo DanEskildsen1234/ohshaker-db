82b0ceed73409673082cc13c3fb43739936b36af
ip3ly5 <ip3ly5@gmail.com>
2019-12-02 15:00:17 +0100

    updated values

---
+++ api/ingredient/update.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+
+require_once(__DIR__ . '../../admin-connection.php');
+require_once(__DIR__.'../../functions.php');
+
+if(empty($_SESSION['managerID'])) {
+    sendErrorMessage( 'Not logged in [$_SESSION]' , __LINE__); 
+}
+
+if(empty($_POST['field'])){
+    sendErrorMessage( 'Field is required' , __LINE__); 
+}
+
+if ($_POST['field'] !== 'cName') {
+    sendErrorMessage( 'Method not allowed' , __LINE__); 
+}
+
+// Sends error message if empty, with the exception of eShakenStirred and eCubedCrushed.
+if(empty($_POST['value']){
+    sendErrorMessage( 'Value is required' , __LINE__); 
+}
+
+$iCocktailID = $_POST['ingredientID'];
+$sField = htmlspecialchars($_POST['field']);
+$sValue = htmlspecialchars($_POST['value']);
+
+if ($sField == 'cName') {
+    if(strlen($_POST['value']) < 2 || strlen($_POST['value']) > 50){
+        sendErrorMessage( 'Ingredient name min 2 max 50 characters' , __LINE__);
+    }
+}
+
+$db = new DB();
+$con = $db->connect();
+if ($con) {
+    $results = array();
+    $statement = $con->prepare(
+        "UPDATE `tcocktail` SET `$sField` = '$sValue' WHERE `tcocktail`.`nCocktailID` = $iCocktailID");
+    $statement->execute();
+    echo("Success");
+    $stmt = null;
+    $db->disconnect($con);
+}
