661b2217fb39d4bd7682ccd13f7c5fcdfcf21308
ip3ly5 <ip3ly5@gmail.com>
2019-12-02 16:03:12 +0100

    syntax correction

--- api/ingredient/create.php
+++ api/ingredient/create.php
@@ -9,14 +9,14 @@ require_once(__DIR__.'../../functions.php');
 $sName = htmlspecialchars($_POST['name'], ENT_QUOTES); //  ENT_QUOTES allows use of single quotes
 
 if(empty($_SESSION['managerID'])) {
-    sendErrorMessage( 'Not logged in [$_SESSION]' , __LINE__ ); 
+    sendErrorMessage( 'Not logged in [$_SESSION]', __LINE__); 
 }
 
-if(empty( $_POST['name'] ) ){ 
-    sendErrorMessage( 'ingredient name is missing' , __LINE__ ); 
+if(empty( $_POST['name'])){ 
+    sendErrorMessage( 'ingredient name is missing', __LINE__); 
 }
-if(strlen($_POST['name']) < 2 || strlen($_POST['name']) > 50  ){
-    sendErrorMessage( 'ingredient name min 2 max 50 characters' , __LINE__ );
+if(strlen($_POST['name']) < 2 || strlen($_POST['name']) > 50){
+    sendErrorMessage( 'ingredient name min 2 max 50 characters', __LINE__);
 }
 
 $db = new DB();
