00df575d4e0879633d33440c499edd2a50bb74e3
Marijn Pool <15004290+ph00lt0@users.noreply.github.com>
2019-12-03 18:41:04 +0100

    fixed deletion with join

--- api/bartender/delete.php
+++ api/bartender/delete.php
@@ -7,6 +7,10 @@ if ( $_SERVER['REQUEST_METHOD'] !== 'POST' ) {
     sendErrorMessage( 'Method not allowed' , __LINE__ );
 }
 
+if( empty($_POST["bartenderID"]) ) {
+    sendErrorMessage( "bartenderID is required" , __LINE__ );
+}
+
 session_start();
 
 if( empty($_SESSION['managerID']) ) {
@@ -20,9 +24,15 @@ $db = new DB();
 $con = $db->connect();
 
 if ($con) {
-    $cQuery = "DELETE FROM `tbartender` WHERE `nBartenderID` = '$iBartenderID' AND `nBarID` = '$iBarID'";
-    $statement = $con->query($cQuery);
+    $cQuery = "
+               DELETE tbartender FROM tbarbartender 
+               INNER JOIN tbartender ON tbarbartender.nBartenderID = tbartender.nBartenderID 
+               WHERE tbartender.nBartenderID = '$iBartenderID' AND tbarbartender.nBarID = '$iBarID';
+               ";
+    $statement = $con->prepare($cQuery);
+    $statement->execute();
     $statement = null;
+
     $db->disconnect($con);
 
     sendSuccessMessage( 'Bartender has been removed' , __LINE__ );
