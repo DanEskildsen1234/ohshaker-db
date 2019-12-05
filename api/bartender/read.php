d33f9dbe4d0b998eaed3777bfa99d90e806ff230
DanEskildsen <DanEskildsen1234@users.noreply.github.com>
2019-12-05 18:56:08 +0100

    added functions file, cleaned up minor things and switched prepared stmt to query

--- api/bartender/read.php
+++ api/bartender/read.php
@@ -1,6 +1,6 @@
 <?php
-
 require_once(__DIR__.'../../readonly-connection.php');
+require_once(__DIR__.'../../functions.php');
 
 if( $_SERVER['REQUEST_METHOD'] !== 'POST' ) {
     sendErrorMessage( 'Method not allowed' , __LINE__ );
@@ -11,25 +11,20 @@ session_start();
 if( empty($_SESSION['managerID']) ) {
     sendErrorMessage( 'Not authenticated' , __LINE__ );
 }
-
+/*
 if( empty($_SESSION['barID']) ) {
     sendErrorMessage( 'Corrupt session: barID is not defined' , __LINE__ );
-}
+}*/
 
-$iBarID = (int)htmlspecialchars($_SESSION['barID']);
+$iBarID = (int)($_SESSION['barID']);
 
 $db = new DB();
 $con = $db->connect();
 if ($con) {
-    $results = array();
-
-    $statement = $con->prepare(
-        "SELECT * FROM tbartender bt
-                    INNER JOIN tbarbartender bbt
-                        ON bbt.nBartenderID = bt.nBartenderID
-                    WHERE bbt.nBarID = ?;
-                 ");
-    $statement->execute([$iBarID]);
+    $statement = $con->query(
+    "SELECT * FROM tbartender bt
+    INNER JOIN tbarbartender bbt ON bbt.nBartenderID = bt.nBartenderID 
+    WHERE bbt.nBarID = '$iBarID'");
 
     $results = $statement->fetchAll();
 
