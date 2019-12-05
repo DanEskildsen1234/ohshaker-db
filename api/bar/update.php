0bde94d91ef7bf4cabe44636d1a2de42f46f183a
DanEskildsen <DanEskildsen1234@users.noreply.github.com>
2019-12-05 11:49:47 +0100

    inital bar update.php file

---
+++ api/bar/update.php
@@ -0,0 +1,44 @@
+<?php
+require_once(__DIR__ . '../../admin-connection.php');
+require_once(__DIR__.'../../functions.php');
+require_once(__DIR__ . '../../validation.php');
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    sendErrorMessage( 'Method not allowed' , __LINE__ );
+}
+
+if( empty($_POST['barName']) ) {
+    sendErrorMessage('barName is required' , __LINE__ );
+}
+
+session_start();
+if( empty($_SESSION['managerID']) ) {
+    sendErrorMessage( 'Not authenticated' , __LINE__ );
+}
+if( empty($_SESSION['barID']) ) {
+    sendErrorMessage( 'Corrupt session: barID is not defined' , __LINE__ );
+}
+
+$iManagerID = $_SESSION['managerID'];
+$iBarID = (int)htmlspecialchars($_SESSION['barID']);
+$sBarName = $_POST['barName'];
+
+validateFirstName($sBarName);
+
+$queryValue = htmlspecialchars($sBarName);
+$db = new DB();
+$con = $db->connect();
+if ($con) {
+    $results = array();
+    $statement = $con->prepare(
+        "
+                   UPDATE tbar
+                    INNER JOIN tmanager
+                        ON tmanager.nBarID = tbar.nBarID 
+                    SET `cName` = '$sBarName'
+                    WHERE tmanager.nManagerID = '$iManagerID' AND tbar.nBarID = '$iBarID';
+                 ");
+    $statement->execute();
+    $stmt = null;
+    $db->disconnect($con);
+    sendSuccessMessage( 'Bar name has been updated' , __LINE__ );
+}
