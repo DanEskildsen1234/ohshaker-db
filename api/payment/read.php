5b7400d1b2033162a2dbb9aa1a9f4a7663f369c0
DanEskildsen <DanEskildsen1234@users.noreply.github.com>
2019-12-06 13:40:06 +0100

    removed htmlspecialchars from int variable

--- api/payment/read.php
+++ api/payment/read.php
@@ -8,12 +8,16 @@ if(empty($_SESSION['managerID'])) {
     sendErrorMessage('Not authenticated' , __LINE__);
 }
 
-$iManagerID = (int)htmlspecialchars($_SESSION['managerID']);
+$iManagerID = (int)$_SESSION['managerID'];
 
 $db = new DB();
 $con = $db->connect();
 if ($con) {
-    $statement = $con->query("SELECT tpayment.nPaymentID, tpayment.nCreditCardID, tpayment.nAmount, tpayment.dPayment FROM tpayment INNER JOIN tcreditcard ON tcreditcard.nCreditCardID = tpayment.nCreditCardID INNER JOIN tmanager ON tmanager.nManagerID = tcreditcard.nManagerID WHERE tmanager.nManagerID = $iManagerID");
+    $statement = $con->query(
+    "SELECT tpayment.nPaymentID, tpayment.nCreditCardID, tpayment.nAmount, tpayment.dPayment FROM tpayment 
+    INNER JOIN tcreditcard ON tcreditcard.nCreditCardID = tpayment.nCreditCardID
+    INNER JOIN tmanager ON tmanager.nManagerID = tcreditcard.nManagerID
+    WHERE tmanager.nManagerID = $iManagerID");
     $results = $statement->fetchAll();
     $statement = null;
     
