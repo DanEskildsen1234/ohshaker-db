e6517c3640ed76a6f98a117a5b2325ae3761a9f0
DanEskildsen <DanEskildsen1234@users.noreply.github.com>
2019-12-06 13:26:26 +0100

    replaced prepared statement with 'query' (cocktail)

--- api/cocktail/read.php
+++ api/cocktail/read.php
@@ -1,14 +1,13 @@
 <?php
-
 require_once(__DIR__.'../../readonly-connection.php');
+
 $db = new DB();
 $con = $db->connect();
 if ($con) {
-    $results = array();
-    $statement = $con->prepare("SELECT * FROM tcocktail");
-    $statement->execute();
+    $statement = $con->query("SELECT * FROM tcocktail");
     $results = $statement->fetchAll();
-    print_r(json_encode($results));
-    $stmt = null;
+    $statement = null;
+    
     $db->disconnect($con);
+    echo json_encode($results);
 }
