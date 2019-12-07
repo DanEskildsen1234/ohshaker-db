abf1a563302a00107ca6ae9ad1185ba9141c11a7
ip3ly5 <ip3ly5@gmail.com>
2019-12-07 13:28:28 +0100

    added read functionality for one cocktail or all cocktail

--- api/cocktail/read.php
+++ api/cocktail/read.php
@@ -1,14 +1,33 @@
 <?php
 require_once(__DIR__.'../../restricted-connection.php');
 
+// Sometimes in our app we want to read all cocktails or only one, the api only assigns $sCocktailID variable
+// if there is a post, preventing an undefined error. Later if the variable is truthy, the api will only read
+// that variable, if not it will read all cocktails.
+
+if ($_POST) {
+    $sCocktailID = htmlspecialchars($_POST['cocktailID'], ENT_QUOTES);
+}
+
 $db = new DB();
 $con = $db->connect();
 if ($con) {
-    $results = array();
-    $statement = $con->prepare("SELECT * FROM tcocktail");
-    $statement->execute();
-    $results = $statement->fetchAll();
-    $stmt = null;
+
+    if (!empty($sCocktailID)) {
+        $results = array();
+        $statement = $con->prepare("SELECT * FROM tcocktail WHERE nCocktailID = ?");
+        $statement->execute([$sCocktailID]);
+        $results = $statement->fetchAll();
+    }
+    
+    else {
+        $results = array();
+        $statement = $con->prepare("SELECT * FROM tcocktail");
+        $statement->execute();
+        $results = $statement->fetchAll();
+    }
+
+    $statement = null;
     $db->disconnect($con);
     echo json_encode($results);
 }
