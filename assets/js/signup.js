931bd5978919814d17d36b247a2d727d354d280e
Marijn Pool <15004290+ph00lt0@users.noreply.github.com>
2019-12-08 12:49:43 +0100

    convert cc experiy date input

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,18 +1,26 @@
-function PostManagerSignup() {
-    console.log('submitted');
+async function PostManagerSignUp() {
     const url = 'api/manager/create.php';
     const method = 'POST';
 
     const data = {};
 
-    document.querySelectorAll('[data-sign-up-for] input').forEach( (input) => {
-
+    document.querySelectorAll('[data-sign-up-form] input').forEach( (input) => {
+        let value = input.value;
+        if (input.id === "expiration") {
+            value = encodeExpiryDate(value);
+        }
+        data[input.id] = value;
     });
+
+    console.log(data);
+
+    const response = await fetchData(url, data, method);
+    console.log(response);
 }
 
 window.addEventListener('DOMContentLoaded', (event) => {
     document.querySelector('[data-create]').addEventListener('click', (e) => {
         e.preventDefault();
-        PostManagerSignup();
+        PostManagerSignUp();
     }, false)
 });
--- signup.php
+++ signup.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Sign up</title>
+    <script src="assets/js/functions.js"></script>
     <script src="assets/js/signup.js"></script>
 </head>
 <body>
@@ -16,10 +17,14 @@
         <input placeholder="Surname" id="surname" type="text">
         <input placeholder="Username" id="username" type="text">
         <input placeholder="Email" id="email" type="email">
-        <input placeholder="Phone number" id="phonenumber" type="tel">
+        <input placeholder="Phone number" id="phoneNumber" type="tel">
         <input placeholder="Address" id="address" type="text">
         <input placeholder="Zip" id="zip" type="text">
         <input placeholder="Password" id="password" type="password">
+        <input placeholder="IBAN" id="IBAN" type="text">
+        <input placeholder="CCV" id="CCV" type="number">
+        <input placeholder="MM/YY" id="expiration" type="month">
+
         <button data-create>Sign up</button>
     </form>
 </section>
