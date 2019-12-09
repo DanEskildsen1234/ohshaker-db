0c2eb962386ec6b1c62a9f59f3e69f4c0b7a9b28
DanEskildsen <DanEskildsen1234@users.noreply.github.com>
2019-12-09 11:07:10 +0100

    inital card files

---
+++ assets/js/card.js
@@ -0,0 +1,27 @@
+async function postExistingPayments() { 
+    const url = 'api/card/read.php'; // these fields are passed to the api
+    const method = "POST";
+    const data = {};
+
+    const response = JSON.parse(await fetchData(url, data, method)); // response recieved from api
+
+    const existingCardSection = document.querySelector("[data-existing-cards]");
+        // Get template
+        const existingCardTemp = document.querySelector("[data-existing-cards-template]").content;
+        // For every object in the response
+        response.forEach( (card)=> {
+            // Create a clone
+            const cln = existingCardTemp.cloneNode(true);
+            // For each item in the object
+            for (let field in card) {
+                if (field === "nCardID") {
+                    cln.querySelector("div").id = card[field];
+                } else {
+                    cln.querySelector(`#${field}`).innerText = card[field];
+                    //cln.querySelector(`#${field}`).setAttribute('disabled', true); // disables editing
+                }
+            }
+            // Release clone on frontend
+            existingPaymentSection.appendChild(cln);
+        })
+}
