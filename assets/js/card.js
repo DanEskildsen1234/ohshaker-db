448e7c7bf84fd5a13d56da5034158829dadcf6f5
DanEskildsen <DanEskildsen1234@users.noreply.github.com>
2019-12-09 15:07:39 +0100

    insert working, wip date field format

--- assets/js/card.js
+++ assets/js/card.js
@@ -1,3 +1,36 @@
+async function postCard() {
+    let expDate = document.querySelector('[data-expDate]').value;
+    let CCV = document.querySelector('[data-CCV]').value;
+    let IBAN = document.querySelector('[data-IBAN]').value;
+
+    const url = 'api/card/create.php';
+    const method = "POST";
+    const data = {"expiration": expDate, "CCV": CCV, "IBAN": IBAN};
+
+    const response = JSON.parse(await fetchData(url, data, method));
+    console.log(response);
+
+    if (response.status === 0) {
+        document.querySelector('[data-error]').innerText = response.message;
+    }
+    
+    if (response.status === 1) {
+        document.querySelector('[data-error]').innerText = "";
+        document.querySelector('[data-success]').innerText = response.message;
+    }
+}
+
+window.addEventListener('DOMContentLoaded', (event) => { // wait for dom content to load
+const cardButton = document.querySelector('[data-btnAddCard]');
+
+if (cardButton) {
+    cardButton.addEventListener('click', () => {
+        postCard();
+        console.log('hej')
+    }, false);
+}
+});
+
 async function postExistingCard() { 
     const url = 'api/card/read.php'; // these fields are passed to the api
     const method = "POST";
@@ -16,6 +49,8 @@ async function postExistingCard() {
             for (let field in card) {
                 if (field === "nCreditCardID") {
                     cln.querySelector("div").id = card[field];
+                } else if (field === "nManagerID") {
+                    cln.querySelector(`#${field}`).classList.add('hidden');
                 } else {
                     cln.querySelector(`#${field}`).innerText = card[field];
                     //cln.querySelector(`#${field}`).setAttribute('disabled', true); // disables editing
--- creditcards.php
+++ creditcards.php
@@ -8,16 +8,18 @@
     <title>Credit cards</title>
 </head>
 <body> 
+<!-- validate manager -->
     <div id="card-field-input">
         <span data-error class="error-box"></span>
         <span data-success class="success-box"></span>
-        <input data-cardID name="cardID" type="number" placeholder="Credit Card ID">
-        <button data-payBtn id="btnPay"><b>Renew Subscription</b></button>
+
+        <input data-expDate name="expiration" type="text" placeholder="Expiration date">
+        <input data-CCV name="CCV" type="number" placeholder="CCV">
+        <input data-IBAN name="IBAN" type="text" placeholder="IBAN">
+        <button data-btnAddCard id="btnAddCard"><b>Add credit card</b></button>
     </div>
 
     <div>
-        <div class="card-field"><p>Credit card ID</p></div>
-        <div class="card-field"><p>Manager ID</p></div>
         <div class="card-field"><p>Total payment amount</p></div>
         <div class="card-field"><p>Expiration date</p></div>
         <div class="card-field"><p>CCV</p></div>
@@ -27,7 +29,6 @@
     <section id="existing-cards" data-existing-cards>
         <template data-existing-cards-template>
             <div>
-                <div class="card-field" id="nCreditCardID"></div>
                 <div class="card-field" id="nManagerID"></div>
                 <div class="card-field" id="nTotalAmount"></div>
                 <div class="card-field" id="dExpiration"></div>
