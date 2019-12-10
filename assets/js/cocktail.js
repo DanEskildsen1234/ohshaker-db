f3fe6cd496db88b746f4b3cf8d6cd3d9f8ef6b9d
Marijn Pool <15004290+ph00lt0@users.noreply.github.com>
2019-12-10 15:58:26 +0100

    Update cocktail.js

--- assets/js/cocktail.js
+++ assets/js/cocktail.js
@@ -1,13 +1,13 @@
 let cocktailsPage = document.querySelector('#cocktailPage');
-if (cocktailsPage){
 
+if (cocktailsPage){
     async function getCocktails() {
         const url = "api/cocktail/read.php";
         const data = {};
         const method = "POST";
 
         const response = JSON.parse(await fetchData(url, data, method));
-        
+
         for( let key in response ){
             let cocktailName = document.createElement("a");
             document.getElementById("cocktail").appendChild(cocktailName);
@@ -22,8 +22,8 @@ if (cocktailsPage){
 
 let singlePage = document.querySelector('#singlePage');
 let editPage = document.querySelector('#editPage');
-if (singlePage || editPage){
 
+if (singlePage || editPage){
     async function getGlobalIngredients() {
         let url = "api/ingredient/read.php";
         let data = {};
@@ -31,15 +31,15 @@ if (singlePage || editPage){
         let response = JSON.parse(await fetchData(url, data, method));
 
         response.forEach((ingredient) => {
+            
             if(editPage){
-
                 var ingredientOption = document.createElement("option");
                 ingredientOption.innerHTML = ingredient.cName;
                 ingredientOption.value = ingredient.nIngredientID;
 
                 document.getElementById("insert-ingredient").appendChild(ingredientOption);
-
             }
+            
         })
     }
 
@@ -50,17 +50,14 @@ if (singlePage || editPage){
         let method = "POST";
         let response = JSON.parse(await fetchData(url, data, method));
         
-        if (singlePage){
-        document.getElementById("edit").setAttribute('href', 'cocktailedit.php?id='+response.nCocktailID);
+        if (singlePage) {
+            document.getElementById("edit").setAttribute('href', 'cocktailedit.php?id='+response.nCocktailID);
         }
         
         for (let field in response) {
-
-            
-
             if (isNaN(field) && field != 'nCocktailID' && field != 'ingredients') {
-
                 document.getElementById(field).innerText = response[field];
+                
                 if (editPage){
                     document.getElementById(field).value = response[field];
                 }
@@ -76,28 +73,26 @@ if (singlePage || editPage){
             const cln = ingredientTemp.cloneNode(true);
 
             for (let field in ingredient) {
-                            if (field != 'nIngredientID'){
-                                cln.querySelector(`#${field}`).innerHTML = ingredient[field];
-                            }
-                            if (editPage && field != 'nIngredientID'){
-                                cln.querySelector(`#${field}`).value = ingredient[field];
-                                cln.querySelector(`#${field}`).className = ingredient.nIngredientID;
-                                cln.querySelector('#remove-ingredient').value = ingredient.nIngredientID;
-                                // query selector for updating field
-                                cln.querySelector(`#${field}`).addEventListener('blur', event => {
-                                     postUpdateCocktail();
-                                })
-                            } 
-
-                            
-                        }
-                        if (editPage){
-                    cln.querySelector(`#remove-ingredient`).addEventListener('click', event => {
-                        postUpdateCocktail();
+                if (field != 'nIngredientID'){
+                    cln.querySelector(`#${field}`).innerHTML = ingredient[field];
+                }
+                if (editPage && field != 'nIngredientID'){
+                    cln.querySelector(`#${field}`).value = ingredient[field];
+                    cln.querySelector(`#${field}`).className = ingredient.nIngredientID;
+                    cln.querySelector('#remove-ingredient').value = ingredient.nIngredientID;
+                    // query selector for updating field
+                    cln.querySelector(`#${field}`).addEventListener('blur', event => {
+                         postUpdateCocktail();
                     })
-                    }
-                    updateIngredientSection.appendChild(cln);
-                });    
+                } 
+            }
+            if (editPage){
+                cln.querySelector(`#remove-ingredient`).addEventListener('click', event => {
+                    postUpdateCocktail();
+                })
+            }
+            updateIngredientSection.appendChild(cln);
+        });    
     }
     getGlobalIngredients();
     getCocktail();
@@ -106,51 +101,48 @@ if (singlePage || editPage){
         item.addEventListener('blur', event => {
             postUpdateCocktail();
         })
-      })
-      
-      document.getElementById("add-ingredient").addEventListener('click', event => {
+     })
+    
+    document.getElementById("add-ingredient").addEventListener('click', event => {
         var dropdown = document.getElementById("insert-ingredient");
         var dropdownValue = dropdown.options[dropdown.selectedIndex].value;
 
-        console.log(dropdownValue);
-
         document.getElementById("add-ingredient").value = dropdownValue;
           postUpdateCocktail();
-      })
+        })
 
-    async function postUpdateCocktail() {
-        let searchParams = new URLSearchParams(document.location.search);
-        let measurement = document.getElementById("measurement").value;
-        let measurementType = document.getElementById("measurementType").value;
-        const url = 'api/cocktail/update.php';
-        const method = 'POST';
-        const data = {"cocktailID": searchParams.get('id'),
-                      "field": event.target.id,
-                      "value":event.target.value,
-                      "measurement":measurement,
-                      "measurementType": measurementType
-                    };
+        async function postUpdateCocktail() {
+            let searchParams = new URLSearchParams(document.location.search);
+            let measurement = document.getElementById("measurement").value;
+            let measurementType = document.getElementById("measurementType").value;
+            const url = 'api/cocktail/update.php';
+            const method = 'POST';
+            const data = {"cocktailID": searchParams.get('id'),
+                          "field": event.target.id,
+                          "value":event.target.value,
+                          "measurement":measurement,
+                          "measurementType": measurementType
+                         };
 
-                    if(event.target.id == "measurement" || event.target.id == "measurementType"){
-                        return; 
-                    }
+            if(event.target.id == "measurement" || event.target.id == "measurementType"){
+                return; 
+            }
 
-                    if (event.target.id == 'remove-ingredient'){
-                    var elem = event.target.parentNode;
-                    elem.remove();
-                }
+            if (event.target.id == 'remove-ingredient'){
+                var elem = event.target.parentNode;
+                elem.remove();
+            }
 
-                console.log(data);
-                fetchData(url, data, method);
-                // const response = JSON.parse(await fetchData(url, data, method));
-                // messageBox(response);
-                if (event.target.id == 'add-ingredient'){
-                    location.reload();
-                }
+            fetchData(url, data, method);
+            // const response = JSON.parse(await fetchData(url, data, method));
+            
+            // messageBox(response);
+            if (event.target.id == 'add-ingredient'){
+                location.reload();
+            }
     }
 
     async function createCocktail() {
-
         const url = "api/cocktail/create.php";
         const data = {};
         const method = "POST";
