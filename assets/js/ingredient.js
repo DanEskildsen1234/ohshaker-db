10d45cfdc40354a852960ed5fa80325a45dbbbd3
ip3ly5 <ip3ly5@gmail.com>
2019-12-10 11:08:16 +0100

    ingredient page read functionality

---
+++ assets/js/ingredient.js
@@ -0,0 +1,37 @@
+let ingredientsPage = document.querySelector('#ingredientsPage');
+
+if (ingredientsPage){
+async function getIngredients() { 
+    console.log("yep");
+    const url = 'api/ingredient/read.php'; // these fields are passed to the api
+    const method = "POST";
+    const data = {};
+    
+    const response = JSON.parse(await fetchData(url, data, method)); 
+    // response recieved from api
+    console.log(response);
+    const ingredientSection = document.querySelector("[data-ingredient-section]");
+        // Get template
+        const ingredientTemp = document.querySelector("[data-ingredient-template]").content;
+        // For every object in the response
+        console.log(ingredientTemp)
+        response.forEach( (ingredient)=> {
+            console.log(ingredient);
+            // Create a clone
+            const cln = ingredientTemp.cloneNode(true);
+            // For each item in the object
+            for (let field in ingredient) {
+                if (field === "nIngredientID") {
+                    cln.querySelector(".ingredientSection").id = ingredient[field];
+                } else {
+                    cln.querySelector(`#${field}`).innerText = ingredient[field];
+                }
+            }
+            ingredientSection.appendChild(cln);
+        })
+            // Release clone on frontend
+    }
+
+   deleteIngredient = document.querySelector(".delete-ingredient");
+    getIngredients();
+}
---
+++ ingredient.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ingredient</title>
+</head>
+<body> 
+<span id="ingredientsPage"><span>
+    <section id="ingredient-section" data-ingredient-section>
+        <template data-ingredient-template>
+            <div class="ingredientSection">
+                <div style="display:inline" class="ingredient-field" id="cName"></div>
+                <div style="display:inline" class="delete-ingredient">Delete</div>
+            </div>
+        </template>
+    </section>
+
+    <script src="assets/js/functions.js"></script>
+    <script src="assets/js/ingredient.js"></script>
+</body>
+</html>
